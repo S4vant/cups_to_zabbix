3101cb40e64c9379621de63193bf50a7caae086b
S4vant <ryo.66645@gmail.com>
2026-05-26 10:14:41 +0400

    4 commit

--- parser.py
+++ parser.py
@@ -2,21 +2,26 @@
 
 import re
 import json
+import subprocess
 import socket
 from collections import defaultdict
 
-LOGFILE = "cups.log"   # сюда вставить файл с логами
+LOGFILE = "/tmp/cups.log"
+
+HOST = "cups-demo"
+SERVER = "localhost"
+
+pattern = re.compile(
+    r"^(.*?)\s+([\w\.-]+)\s+(\d+)\s+\[(.*?)\]\s+total\s+(\d+)\s+-\s+localhost\s+(.*?)\s+"
+)
+
+users = defaultdict(int)
+pages_total = 0
+jobs_count = 0
+jobs_raw = []
 
 hostname = socket.gethostname()
 
-result = {
-    "host": hostname,
-    "users": {}
-}
-
-pattern = re.compile(
-    r"^(.*?)\s+([\w\.-]+)\s+(\d+)\s+(.*)$"
-)
 
 with open(LOGFILE, encoding="utf-8") as f:
     for line in f:
@@ -26,31 +31,76 @@ with open(LOGFILE, encoding="utf-8") as f:
         if not m:
             continue
 
-        printer_job = m.group(1)
+        printer = m.group(1)
         user = m.group(2)
-        size = int(m.group(3))
-        date = m.group(4)
+        job_id = m.group(3)
+        time = m.group(4)
+        pages = int(m.group(5))
+        file_name = m.group(6).split(" - ")[0]
 
-        # выделяем job_id
-        job_match = re.search(r'-(\d+)$', printer_job)
+        users[user] += 1
+        pages_total += pages
+        jobs_count += 1
 
-        job_id = job_match.group(1) if job_match else "unknown"
-
-        printer = re.sub(r'-\d+$', '', printer_job)
-
-        entry = {
-            "job_id": job_id,
+        jobs_raw.append({
             "printer": printer,
-            "file": "unknown",
-            "pages": "unknown",
-            "bytes": size,
-            "date": date
-        }
-
-        if user not in result["users"]:
-            result["users"][user] = []
-
-        result["users"][user].append(entry)
+            "user": user,
+            "job_id": job_id,
+            "pages": pages,
+            "file": file_name,
+            "time": time
+        })
 
 
-print(json.dumps(result, indent=4, ensure_ascii=False))
+payload = {
+    "host": hostname,
+    "users": list(users.keys()),
+    "jobs_count": jobs_count,
+    "pages_total": pages_total,
+    "jobs": jobs_raw
+}
+
+json_data = json.dumps(payload, ensure_ascii=False)
+
+
+subprocess.run([
+    "zabbix_sender",
+    "-z", SERVER,
+    "-p", "10051",
+    "-s", HOST,
+    "-k", "cups.jobs.raw",
+    "-o", json_data
+])
+
+
+subprocess.run([
+    "zabbix_sender",
+    "-z", SERVER,
+    "-p", "10051",
+    "-s", HOST,
+    "-k", "cups.users",
+    "-o", ",".join(users.keys())
+])
+
+
+subprocess.run([
+    "zabbix_sender",
+    "-z", SERVER,
+    "-p", "10051",
+    "-s", HOST,
+    "-k", "cups.jobs.count",
+    "-o", str(jobs_count)
+])
+
+
+subprocess.run([
+    "zabbix_sender",
+    "-z", SERVER,
+    "-p", "10051",
+    "-s", HOST,
+    "-k", "cups.pages.total",
+    "-o", str(pages_total)
+])
+
+
+print("sent")
